9bc128db06d4e4bc7e8a3e46156f480fea11652a
Saves Paul <paul.saves@alumni.enac.fr>
2026-06-27 22:41:29 -0400

    Feat/sur (#1)
    * feat: implement IMSE acquisition function
    
    Adds the integrated mean squared error (IMSE) acquisition function for both KRG and MFK models using Cholesky variance updates. Includes unit tests, integration tests, and a tutorial notebook.
    
    * fix
    
    * fix
    
    * update
    
    * test: normalize IMSE convergence entropy to 1 max
    
    * fix
    
    * docs: add 3D space-filling convergence test to IMSE notebook
    
    * fix

---
+++ src/tests/test_integration/test_imse.py
@@ -0,0 +1,54 @@
+import unittest
+import numpy as np
+from smt.applications import MFK
+from smt_optim.acquisition_functions.integrated_mean_squared_error import integrated_mean_squared_error
+
+class TestIMSEIntegration(unittest.TestCase):
+    def setUp(self):
+        np.random.seed(42)
+        # 1D test problem for MFK
+        self.xt_c = np.array([[0.0], [0.25], [0.5], [0.75], [1.0]])
+        self.yt_c = np.array([[0.0], [0.5], [1.0], [0.5], [0.0]])
+        
+        self.xt_e = np.array([[0.0], [0.5], [1.0]])
+        self.yt_e = np.array([[0.0], [1.0], [0.0]])
+        
+        self.sm = MFK(theta0=[1e-2], print_global=False)
+        self.sm.set_training_values(self.xt_c, self.yt_c, name=0)
+        self.sm.set_training_values(self.xt_e, self.yt_e)
+        self.sm.train()
+        
+        # Integration points
+        self.x_mc = np.linspace(0, 1, 50).reshape(-1, 1)
+
+    def test_imse_mfk(self):
+        # Candidate point
+        x_cand = np.array([[0.5]])
+        
+        imse_val = integrated_mean_squared_error(
+            self.sm, 
+            points=x_cand, 
+            integration_points=self.x_mc, 
+            inv_block=True
+        )
+        
+        self.assertEqual(imse_val.shape, (1, 1))
+        self.assertGreaterEqual(imse_val[0, 0], 0.0)
+        
+    def test_vec_imse_mfk(self):
+        # Multiple candidate points
+        x_cands = np.array([[0.25], [0.75]])
+        
+        imse_vals = integrated_mean_squared_error(
+            self.sm, 
+            points=x_cands, 
+            integration_points=self.x_mc, 
+            inv_block=True
+        )
+        
+        self.assertEqual(imse_vals.shape, (2, 1))
+        self.assertGreaterEqual(imse_vals[0, 0], 0.0)
+        self.assertGreaterEqual(imse_vals[1, 0], 0.0)
+
+if __name__ == "__main__":
+    unittest.main()
