347ff14b183a382525f2c68436c7f611e743bba9
Paul Saves <paul.saves@alumni.enac.fr>
2026-07-04 17:07:38 -0400

    test: add 1D inputs to benchmark tests for 100% code coverage

--- src/tests/test_unit/test_mo_benchmarks.py
+++ src/tests/test_unit/test_mo_benchmarks.py
@@ -6,87 +6,103 @@ from smt_optim.benchmarks.multi_obj import BNH, TNK, OSY, DTLZ5
 class TestMOBenchmarks(unittest.TestCase):
     def test_bnh(self):
         prob = BNH()
-        x = np.array([
+        x2d = np.array([
             [0.0, 0.0],
             [1.0, 1.0]
         ])
-        f1 = prob.f1(x)
-        f2 = prob.f2(x)
-        g1 = prob.g1(x)
-        g2 = prob.g2(x)
-
-        np.testing.assert_allclose(f1, [0.0, 8.0])
-        np.testing.assert_allclose(f2, [50.0, 32.0])
-        np.testing.assert_allclose(g1, [0.0, -8.0])
-        np.testing.assert_allclose(g2, [-65.3, -57.3])
+        x1d = np.array([0.0, 0.0])
+        
+        np.testing.assert_allclose(prob.f1(x2d), [0.0, 8.0])
+        np.testing.assert_allclose(prob.f1(x1d), 0.0)
+        
+        np.testing.assert_allclose(prob.f2(x2d), [50.0, 32.0])
+        np.testing.assert_allclose(prob.f2(x1d), 50.0)
+        
+        np.testing.assert_allclose(prob.g1(x2d), [0.0, -8.0])
+        np.testing.assert_allclose(prob.g1(x1d), 0.0)
+        
+        np.testing.assert_allclose(prob.g2(x2d), [-65.3, -57.3])
+        np.testing.assert_allclose(prob.g2(x1d), -65.3)
 
     def test_tnk(self):
         prob = TNK()
-        x = np.array([
+        x2d = np.array([
             [1.0, 1.0]
         ])
-        f1 = prob.f1(x)
-        f2 = prob.f2(x)
-        g1 = prob.g1(x)
-        g2 = prob.g2(x)
-
-        np.testing.assert_allclose(f1, [1.0])
-        np.testing.assert_allclose(f2, [1.0])
+        x1d = np.array([1.0, 1.0])
+        
+        np.testing.assert_allclose(prob.f1(x2d), [1.0])
+        np.testing.assert_allclose(prob.f1(x1d), 1.0)
+        
+        np.testing.assert_allclose(prob.f2(x2d), [1.0])
+        np.testing.assert_allclose(prob.f2(x1d), 1.0)
+        
         expected_g1 = -1.0 - 1.0 + 1.0 + 0.1 * np.cos(16.0 * np.arctan(1.0))
-        np.testing.assert_allclose(g1, [expected_g1])
-        np.testing.assert_allclose(g2, [0.0])
+        np.testing.assert_allclose(prob.g1(x2d), [expected_g1])
+        np.testing.assert_allclose(prob.g1(x1d), expected_g1)
+        
+        np.testing.assert_allclose(prob.g2(x2d), [0.0])
+        np.testing.assert_allclose(prob.g2(x1d), 0.0)
 
     def test_osy(self):
         prob = OSY()
-        x = np.array([
+        x2d = np.array([
             [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
             [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
         ])
-        f1 = prob.f1(x)
-        f2 = prob.f2(x)
-        g1 = prob.g1(x)
-        g2 = prob.g2(x)
-        g3 = prob.g3(x)
-        g4 = prob.g4(x)
-        g5 = prob.g5(x)
-        g6 = prob.g6(x)
-
-        # f1: -(25*(x0-2)^2 + (x1-2)^2 + (x2-1)^2 + (x3-4)^2 + (x4-1)^2)
-        # for x=0: -(25*4 + 4 + 1 + 16 + 1) = -122
-        # for x=1: -(25*1 + 1 + 0 + 9 + 0) = -35
-        np.testing.assert_allclose(f1, [-122.0, -35.0])
-        np.testing.assert_allclose(f2, [0.0, 6.0])
-        np.testing.assert_allclose(g1, [2.0, 0.0])
-        np.testing.assert_allclose(g2, [-6.0, -4.0])
-        np.testing.assert_allclose(g3, [-2.0, -2.0])
-        np.testing.assert_allclose(g4, [-2.0, -4.0])
-        np.testing.assert_allclose(g5, [5.0, 1.0])
-        np.testing.assert_allclose(g6, [-5.0, -1.0])
+        x1d = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        
+        np.testing.assert_allclose(prob.f1(x2d), [-122.0, -35.0])
+        np.testing.assert_allclose(prob.f1(x1d), -122.0)
+        
+        np.testing.assert_allclose(prob.f2(x2d), [0.0, 6.0])
+        np.testing.assert_allclose(prob.f2(x1d), 0.0)
+        
+        np.testing.assert_allclose(prob.g1(x2d), [2.0, 0.0])
+        np.testing.assert_allclose(prob.g1(x1d), 2.0)
+        
+        np.testing.assert_allclose(prob.g2(x2d), [-6.0, -4.0])
+        np.testing.assert_allclose(prob.g2(x1d), -6.0)
+        
+        np.testing.assert_allclose(prob.g3(x2d), [-2.0, -2.0])
+        np.testing.assert_allclose(prob.g3(x1d), -2.0)
+        
+        np.testing.assert_allclose(prob.g4(x2d), [-2.0, -4.0])
+        np.testing.assert_allclose(prob.g4(x1d), -2.0)
+        
+        np.testing.assert_allclose(prob.g5(x2d), [5.0, 1.0])
+        np.testing.assert_allclose(prob.g5(x1d), 5.0)
+        
+        np.testing.assert_allclose(prob.g6(x2d), [-5.0, -1.0])
+        np.testing.assert_allclose(prob.g6(x1d), -5.0)
 
     def test_dtlz5(self):
         prob = DTLZ5()
-        x = np.array([
+        x2d = np.array([
             [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         ])
+        x1d = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
         
-        f1 = prob.f1(x)
-        f2 = prob.f2(x)
-        g = prob.g(x)
-        f1_lf = prob.f1_lf(x)
-        f2_lf = prob.f2_lf(x)
-        g_lf = prob.g_lf(x)
-
-        # For x=0, u(xq) = sum((0-0.5)^2) = 7 * 0.25 = 1.75
-        # f1 = (1 + 1.75) * cos(0) = 2.75
-        # f2 = (1 + 1.75) * sin(0) = 0.0
-        np.testing.assert_allclose(f1, [2.75])
-        np.testing.assert_allclose(f2, [0.0])
-        np.testing.assert_allclose(g, [2.25])
+        np.testing.assert_allclose(prob.f1(x2d), [2.75])
+        np.testing.assert_allclose(prob.f1(x1d), 2.75)
         
-        # f1_lf = (1 + 0.8 * 1.75) * cos(0) = 1 + 1.4 = 2.4
-        np.testing.assert_allclose(f1_lf, [2.4])
-        np.testing.assert_allclose(f2_lf, [0.0])
-        np.testing.assert_allclose(g_lf, [2.4])
+        np.testing.assert_allclose(prob.f2(x2d), [0.0])
+        np.testing.assert_allclose(prob.f2(x1d), 0.0)
+        
+        np.testing.assert_allclose(prob.g(x2d), [2.25])
+        np.testing.assert_allclose(prob.g(x1d), 2.25)
+        
+        np.testing.assert_allclose(prob.f1_lf(x2d), [2.4])
+        np.testing.assert_allclose(prob.f1_lf(x1d), 2.4)
+        
+        np.testing.assert_allclose(prob.f2_lf(x2d), [0.0])
+        np.testing.assert_allclose(prob.f2_lf(x1d), 0.0)
+        
+        np.testing.assert_allclose(prob.g_lf(x2d), [2.4])
+        np.testing.assert_allclose(prob.g_lf(x1d), 2.4)
+        
+        np.testing.assert_allclose(prob.u(x1d[3:]), 1.75)
+        np.testing.assert_allclose(prob.u(x2d[:, 3:]), [1.75])
 
 if __name__ == "__main__":
     unittest.main()
--- src/tests/test_unit/test_zdt.py
+++ src/tests/test_unit/test_zdt.py
@@ -7,68 +7,79 @@ class TestZDTBenchmarks(unittest.TestCase):
     def test_zdt1(self):
         zdt = ZDT1()
         zdt.set_dim(5)
-        # Vectorized test
-        x = np.array([
+        # Vectorized test (2D)
+        x2d = np.array([
             [0.0, 0.0, 0.0, 0.0, 0.0],
             [1.0, 1.0, 1.0, 1.0, 1.0]
         ])
+        # 1D test
+        x1d = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
         
-        f1 = zdt.f1(x)
-        f2 = zdt.f2(x)
+        np.testing.assert_allclose(zdt.f1(x2d), [0.0, 1.0])
+        np.testing.assert_allclose(zdt.f1(x1d), 0.0)
         
-        np.testing.assert_allclose(f1, [0.0, 1.0])
-        # For [0,0,0,0,0]: g = 1, f1 = 0, f2 = 1 * (1 - 0) = 1.0
-        # For [1,1,1,1,1]: g = 1 + 9*(4/4) = 10, f1 = 1, f2 = 10 * (1 - sqrt(1/10))
         expected_f2 = [1.0, 10.0 * (1.0 - np.sqrt(0.1))]
-        np.testing.assert_allclose(f2, expected_f2)
+        np.testing.assert_allclose(zdt.f2(x2d), expected_f2)
+        np.testing.assert_allclose(zdt.f2(x1d), expected_f2[0])
+        
+        # Test individual functions for 1D coverage
+        np.testing.assert_allclose(zdt.g(x1d), 1.0)
+        np.testing.assert_allclose(zdt.g(x2d), [1.0, 10.0])
 
     def test_zdt2(self):
         zdt = ZDT2()
         zdt.set_dim(5)
-        x = np.array([
+        x2d = np.array([
             [0.0, 0.0, 0.0, 0.0, 0.0],
             [1.0, 1.0, 1.0, 1.0, 1.0]
         ])
+        x1d = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
         
-        f1 = zdt.f1(x)
-        f2 = zdt.f2(x)
+        np.testing.assert_allclose(zdt.f1(x2d), [0.0, 1.0])
+        np.testing.assert_allclose(zdt.f1(x1d), 0.0)
         
-        np.testing.assert_allclose(f1, [0.0, 1.0])
-        # For [0,0,0,0,0]: g = 1, f1 = 0, f2 = 1 * (1 - 0) = 1.0
-        # For [1,1,1,1,1]: g = 10, f1 = 1, f2 = 10 * (1 - (1/10)^2) = 10 * 0.99 = 9.9
         expected_f2 = [1.0, 9.9]
-        np.testing.assert_allclose(f2, expected_f2)
+        np.testing.assert_allclose(zdt.f2(x2d), expected_f2)
+        np.testing.assert_allclose(zdt.f2(x1d), expected_f2[0])
+        
+        np.testing.assert_allclose(zdt.g(x1d), 1.0)
+        np.testing.assert_allclose(zdt.g(x2d), [1.0, 10.0])
 
     def test_zdt3(self):
         zdt = ZDT3()
         zdt.set_dim(5)
-        x = np.array([
+        x2d = np.array([
             [0.0, 0.0, 0.0, 0.0, 0.0]
         ])
+        x1d = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
         
-        f1 = zdt.f1(x)
-        f2 = zdt.f2(x)
+        np.testing.assert_allclose(zdt.f1(x2d), [0.0])
+        np.testing.assert_allclose(zdt.f1(x1d), 0.0)
         
-        np.testing.assert_allclose(f1, [0.0])
-        # For [0,0,0,0,0]: g = 1, f1 = 0, f2 = 1 * (1 - 0 - 0) = 1.0
         expected_f2 = [1.0]
-        np.testing.assert_allclose(f2, expected_f2)
+        np.testing.assert_allclose(zdt.f2(x2d), expected_f2)
+        np.testing.assert_allclose(zdt.f2(x1d), expected_f2[0])
+        
+        np.testing.assert_allclose(zdt.g(x1d), 1.0)
+        np.testing.assert_allclose(zdt.g(x2d), [1.0])
 
     def test_zdt4(self):
         zdt = ZDT4()
         zdt.set_dim(10)
-        x = np.array([
+        x2d = np.array([
             [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         ])
+        x1d = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
         
-        f1 = zdt.f1(x)
-        f2 = zdt.f2(x)
+        np.testing.assert_allclose(zdt.f1(x2d), [0.0])
+        np.testing.assert_allclose(zdt.f1(x1d), 0.0)
         
-        np.testing.assert_allclose(f1, [0.0])
-        # For x=0: g = 1 + 10*(9) + sum(-10) = 1 + 90 - 90 = 1.0
-        # f2 = 1.0 * (1.0 - 0) = 1.0
         expected_f2 = [1.0]
-        np.testing.assert_allclose(f2, expected_f2)
+        np.testing.assert_allclose(zdt.f2(x2d), expected_f2)
+        np.testing.assert_allclose(zdt.f2(x1d), expected_f2[0])
+        
+        np.testing.assert_allclose(zdt.g(x1d), 1.0)
+        np.testing.assert_allclose(zdt.g(x2d), [1.0])
 
 if __name__ == "__main__":
     unittest.main()
