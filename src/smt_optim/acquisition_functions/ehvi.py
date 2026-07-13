fix: adjust variance stability threshold to 1e-16

src/smt_optim/acquisition_functions/ehvi.py
@@ -144,7 +144,7 @@ def init_ehvi_2o(state) -> Callable:
             mu[i] = models[i].predict_values(x).item()
             s2_obj = models[i].predict_variances(x).item()
 
-            s[i] = np.sqrt(np.maximum(s2_obj, 1e-20))
+            s[i] = np.sqrt(np.maximum(s2_obj, 1e-16))
 
         return ehvi_2o(mu, s, Y)
 

src/smt_optim/acquisition_functions/mpi.py
@@ -56,7 +56,7 @@ def init_mpi(state) -> Callable:
             mu_obj = state.obj_models[idx].predict_values(x).item()
             s2_obj = state.obj_models[idx].predict_variances(x).item()
 
-            s_obj = np.sqrt(np.maximum(s2_obj, 1e-20))
+            s_obj = np.sqrt(np.maximum(s2_obj, 1e-16))
 
             values *= stats.norm.cdf((mu_obj - pareto_front[:, idx]) / s_obj)
 